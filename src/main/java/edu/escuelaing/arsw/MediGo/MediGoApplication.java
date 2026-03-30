6866b425f6fb043f6f98ca7e5f2f16f7acc7a283
juanalch <lozanochavesjuana@gmail.com>
2026-03-30 15:51:19 -0500

    feat(initialize): initial files

---
+++ src/main/java/edu/escuelaing/arsw/MediGo/MediGoApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.MediGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/arsw/MediGo/MediGoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.MediGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
